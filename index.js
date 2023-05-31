3ca6cc9124e7286e792fadef355b24483fc14265
Vadim Cherepanov <xenos26@gmail.com>
2023-05-31 11:45:32 +0700

    chore: add firebase as cloud backend

--- index.js
+++ index.js
@@ -1,4 +1,11 @@
-const TelegramApi = require('node-telegram-bot-api')
-const token = require('./.token')
+const Telegraf = require('telegraf')
+const functions = require('firebase-functions')
 
-const bot = new TelegramApi(token, { polling: true })
+const bot = new Telegraf(functions.config().telegrambot.key)
+
+bot.hears('hi', ctx => ctx.reply('Hey there'));
+bot.launch();
+
+exports.bot = functions.https.onRequest((req, res) => {
+  bot.handleUpdate(req.body, res)
+})
